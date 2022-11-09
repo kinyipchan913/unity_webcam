e7d9758ef230ebd53b33328850e1fc3db340c69d
kinyipchan913 <100573103+kinyipchan913@users.noreply.github.com>
2022-11-09 15:40:45 +0000

    first commit

---
+++ Assets/Scripts/webcam.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class webcam : MonoBehaviour
+{
+	static WebCamTexture backCam;
+
+	void Start()
+	{
+		if (backCam == null)
+			backCam = new WebCamTexture();
+		
+		GetComponent<Renderer>().material.mainTexture = backCam;
+
+		if (!backCam.isPlaying)
+			backCam.Play();
+	}
+
+	void Update()
+	{
+
+	}
+}
